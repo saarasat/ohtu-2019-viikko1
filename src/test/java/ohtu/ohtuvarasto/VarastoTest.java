657664055cd634afdb8025323b6c6eac04d776a6
Saara Satokangas <saarasat@users.noreply.guthub.com>
2019-10-30 08:07:06 +0200

    One failed test for CircleCI

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -121,7 +121,7 @@ public class VarastoTest {
 
     @Test
     public void ottaminenLisääTilaa() {
-        varasto.lisaaVarastoon(8);
+        varasto.lisaaVarastoon(7);
 
         varasto.otaVarastosta(2);
 
